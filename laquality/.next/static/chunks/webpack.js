3821235eefcb1db3de165701589531deda17bd87
Marco <>
2022-06-27 19:21:13 -0700

    add about card and change footer color

--- laquality/.next/static/chunks/webpack.js
+++ laquality/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b1e4fccc78847e23"; }
+/******/ 		__webpack_require__.h = function() { return "0eaa390fef9b7b79"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- laquality/pages/about.js
+++ laquality/pages/about.js
@@ -1,9 +1,12 @@
 import Layout from "../components/layout"
+import Aboot from "../components/aboot"
 
 function About(){
     return(
         <Layout>
+            <Aboot>
 
+            </Aboot>
         </Layout>
     )
 }
